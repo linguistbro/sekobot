dc3acca7e10b013ec3f58dc999b9bc0670de172b
linguistbro <65777077+linguistbro@users.noreply.github.com>
2020-05-23 00:44:48 +0900

    Add files via upload

---
+++ seko.py
@@ -0,0 +1,23 @@
+#import files
+from flask import Flask, render_template, request
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+app = Flask(__name__)
+
+bot = ChatBot("Seko Bot")
+trainer = ChatterBotCorpusTrainer(bot)
+
+trainer.train(
+    "chatterbot.corpus.english"
+)
+
+@app.route("/")
+def home():    
+    return render_template("home.html") 
+@app.route("/get")
+def get_bot_response():    
+    userText = request.args.get('msg')    
+    return str(bot.get_response(userText)) 
+if __name__ == "__main__":    
+    app.run()
